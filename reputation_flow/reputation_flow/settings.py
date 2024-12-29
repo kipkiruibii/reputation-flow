a001fe64a68317778331a439c31f22d27d5b0b18
kipkiruibii <biihezron1@gmail.com>
2024-12-29 11:34:49 +0300

    Background tasks

--- reputation_flow/reputation_flow/settings.py
+++ reputation_flow/reputation_flow/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL='/login'
+
+STATIC_URL = '/static/'  # Already present, defines the URL for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 # Media files (uploaded by users)
