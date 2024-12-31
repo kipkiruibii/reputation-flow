7263749673577eb6534382ba347358b4e22aad4c
kipkiruibii <biihezron1@gmail.com>
2024-12-31 19:11:57 +0300

    Background tasks

--- reputation_flow/reputation_app/views.py
+++ reputation_flow/reputation_app/views.py
@@ -1100,8 +1100,8 @@ def dashboard(request, company_id):
         'review_wall_link':cm.company_review_link,
         'company_enable_ai':cm.company_enable_ai,
         'company_show_page':cm.company_show_page,
-        # 'company_profile':cpp.p_pic.url if cpp else 'https://pic.onlinewebfonts.com/thumbnails/icons_358304.svg' ,
-        'company_profile': 'https://img.freepik.com/premium-vector/vector-logo-dance-club-that-says-dance-club_1107171-3823.jpg',
+        'company_profile':cpp.p_pic.url if cpp else 'https://pic.onlinewebfonts.com/thumbnails/icons_358304.svg' ,
+        # 'company_profile': 'https://img.freepik.com/premium-vector/vector-logo-dance-club-that-says-dance-club_1107171-3823.jpg',
         'company_about': cm.company_about,
         'company_subs': {
             'subscription_active': cm.company_active_subscription,
