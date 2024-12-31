36f98ac73a069ac59957fdf31e8633c6e119be97
kipkiruibii <biihezron1@gmail.com>
2024-12-31 19:27:44 +0300

    Background tasks

--- reputation_flow/reputation_flow/settings.py
+++ reputation_flow/reputation_flow/settings.py
@@ -147,7 +147,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-AWS_DEFAULT_ACL = 'public-read'
 AWS_QUERYSTRING_AUTH = False  # Disable query string auth for public URLs
 
 STATIC_URL = 'static/'
