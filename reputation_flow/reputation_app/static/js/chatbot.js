d2fb78c4c9b999c9e4eb8c72666584f0b5aa1305
kipkiruibii <biihezron1@gmail.com>
2025-01-06 14:50:27 +0300

    Background tasks

--- reputation_flow/reputation_app/static/js/chatbot.js
+++ reputation_flow/reputation_app/static/js/chatbot.js
@@ -1,12 +1,16 @@
-
 const chatbotIframe = document.getElementById('chatbot-iframe');
 const chatbotIcon = document.getElementById('chatbot-icon-rs');
 
 function toggleIframe() {
-    if (chatbotIframe.style.display === 'none') {
-        chatbotIframe.style.display = 'flex'; // Show iframe
+    var iframe = document.getElementById('chatbot-iframe');
+    // Check if iframe is currently hidden
+    if (iframe.style.display === "none" || iframe.style.display === "") {
+        iframe.style.display = "block";  // Show the iframe
+        iframe.style.visibility = "visible";  // Ensure visibility
+        iframe.style.opacity = "1";  // Make it fully visible
     } else {
-        chatbotIframe.style.display = 'none'; // Hide iframe
+        iframe.style.display = "none";  // Hide the iframe
+        iframe.style.visibility = "hidden";  // Hide visibility
+        iframe.style.opacity = "0";  // Make it invisible
     }
 }
-
