d84af112bf94b4fa44ff3f4a9a3ef640ef768202
kipkiruibii <biihezron1@gmail.com>
2025-01-15 16:04:58 +0300

    Dedicated page

--- reputation_flow/reputation_app/views.py
+++ reputation_flow/reputation_app/views.py
@@ -2459,6 +2459,7 @@ def processInstagramReplies(replies, page_access_token,account_id):
             pass
         created_time_with_timezone = created_time_naive.astimezone(timezone.utc)        
         cpr = CompanyPostsCommentsReplies.objects.filter(comment_id=comment_id).first()
+        print(len(CompanyPostsCommentsReplies.objects.filter(comment_id=comment_id)))
         if not cpr:
             print('cpr present')
             cpr = CompanyPostsCommentsReplies(
@@ -2472,12 +2473,12 @@ def processInstagramReplies(replies, page_access_token,account_id):
                 is_published=True,
                 date_updated=created_time_with_timezone
             )
-            cpr.save()
+            # cpr.save()
         else:
             print('cpr absent')
-            cpr.message = data['message']
+            cpr.message = data['text']
             cpr.like_count = data['like_count']
-            cpr.save()
+            # cpr.save()
 
 
 def fetchFacebookComments(post, post_id):
